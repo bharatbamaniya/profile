c2d51651b452747ddfbb1682bcc4f8853e3ea7c4
Bharat Kumar <bkumarm048@gmail.com>
2021-04-26 18:20:24 +0530

    updated

--- apps/twitterlogin1/autoload.php
+++ apps/twitterlogin1/autoload.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Use to autoload needed classes without Composer.
  *
@@ -6,12 +7,11 @@
  * @return void
  */
 spl_autoload_register(function ($class) {
-
     // project-specific namespace prefix
     $prefix = 'Abraham\\TwitterOAuth\\';
 
     // base directory for the namespace prefix
-    $base_dir = __DIR__ . '/lib/src/';
+    $base_dir = __DIR__ . '/src/';
 
     // does the class use the namespace prefix?
     $len = strlen($prefix);
