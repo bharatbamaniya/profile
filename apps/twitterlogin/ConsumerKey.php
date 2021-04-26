57db0d11988cd48d39b50486495b3034ce5a06ea
Bharat Kumar <bkumarm048@gmail.com>
2021-04-26 18:22:57 +0530

    Update ConsumerKey.php

--- apps/twitterlogin/ConsumerKey.php
+++ apps/twitterlogin/ConsumerKey.php
@@ -1,4 +1,4 @@
 <?php
 define('CONSUMER_KEY', '1jKznQHGqgmYyoRsl5HM7MV26'); // add your app consumer key between single quotes
 define('CONSUMER_SECRET', 'rV8lDo1UHdbMzQMSJW2gAgc73mRia9LU74uqVJXLlX1B8YsBSf'); // add your app consumer secret key between single quotes
-define('OAUTH_CALLBACK', 'https://bharatkumar.ml/apps/twitterlogin/callback.php'); // your app callback URL
+define('OAUTH_CALLBACK', 'https://bharatkumar.ml/apps/twitterlogin/'); // your app callback URL
